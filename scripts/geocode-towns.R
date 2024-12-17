d1f249764796a51ea02c2d42440a96bed53c24a8
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-12-17 22:22:25 +0000

    Built site for gh-pages

--- scripts/geocode-towns.R
+++ scripts/geocode-towns.R
@@ -7,11 +7,17 @@ towns <- tibble::tribble(
   "Newcastle",
   "Jerrys Plains",
   "Windsor",
-  "Emu Plains"
+  "Emu Plains",
+  "Mudgee"
 ) |>
   dplyr::mutate(
     address = paste0(name, ", New South Wales, Australia"),
-    nominatimlite::geo_lite(address, lat = "latitude", long = "longitude")
+    nominatimlite::geo_lite(address, lat = "latitude", long = "longitude"),
+    name = dplyr::case_when(
+      name == "Sydney" ~ "Gadigal/Sydney",
+      name == "Newcastle" ~ "Muloobinba/\nNewcastle",
+      .default = name
+    )
   ) |>
   readr::write_csv(file.path("data", "towns.csv"))
 
